46a76f64e889edd5389da64ba17080d6473fa442
Edgar Lau <laue09@gmail.com>
2013-10-14 22:55:07 -1000

    Attempt-2-2-1

--- app/views/formdata/ContactFormData.java
+++ app/views/formdata/ContactFormData.java
@@ -1,5 +1,9 @@
 package views.formdata;
 
+import java.util.ArrayList;
+import java.util.List;
+import play.data.validation.ValidationError;
+
 /**
  * 
  * @author Edgar
@@ -11,5 +15,34 @@ public class ContactFormData {
   public String lastName = "";
   /**The telephone form field*/
   public String telephone = "";
+
+
+/**
+ * 
+ * @return
+ */
+  public List<ValidationError> validate() {
   
+    List<ValidationError> errors = new ArrayList<>();
+  
+    if(firstName == null || firstName.length() ==0) {
+      errors.add(new ValidationError("firstName", "Please type in first name"));
+    }
+  
+    if(lastName == null || lastName.length() ==0) {
+      errors.add(new ValidationError("lastName", "Please type in last name"));
+    }
+  
+    if(telephone == null || telephone.length() ==0) {
+      errors.add(new ValidationError("telephone", "Please type telephone number"));
+    }
+  
+    if(telephone.length() != 12) {
+      errors.add(new ValidationError("telephone", "Please type telephone number"));
+    }
+  
+  
+    return errors.isEmpty() ? null : errors;
+  }
 }
+
